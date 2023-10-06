a0f10168dcec9badcfb98db7deb45231526b085e
JessicaGreben <jgrebenschikov@aerospike.com>
2023-10-05 17:13:10 -0700

    seed data

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/aerospike/aerospike-client-go/v6"
 )
 
+const (
+	namespace = "aerospike_cloud"
+)
+
+var (
+	forever     bool
+	concurrency int
+	seedData    bool
+)
+
 func main() {
-	forever := flag.Bool("forever", false, "loop forever")
+	flag.BoolVar(&forever, "forever", false, "loops forever reading/writing a single record to the database to ensure connectivity. Defaults to false.")
+	flag.IntVar(&concurrency, "concurrency", 10, "how many concurrent goroutines to use when writing data.")
+	flag.BoolVar(&seedData, "seed-data", false, "seed the database with fake data. Defaults to false.")
 	flag.Parse()
 
 	aerospikeHost := os.Getenv("AEROSPIKE_CLOUD_HOSTNAME")
@@ -42,37 +56,111 @@ func main() {
 	}
 	defer client.Close()
 
-	key, err := aerospike.NewKey("aerospike_cloud", "foo", "bar")
-	if err != nil {
-		log.Println("0: ", err)
+	t := newTester(client)
+	if err := t.run(); err != nil {
+		log.Fatal(err)
 	}
-
-	if *forever {
-		for {
-			do(client, key)
-			time.Sleep(15 * time.Second)
-		}
-	}
-	do(client, key)
 }
 
-func do(client *aerospike.ProxyClient, key *aerospike.Key) {
+type tester struct {
+	asClient    *aerospike.ProxyClient
+	readPolicy  *aerospike.BasePolicy
+	writePolicy *aerospike.WritePolicy
+}
+
+func newTester(client *aerospike.ProxyClient) *tester {
 	writePolicy := aerospike.NewWritePolicy(0, 0)
 	writePolicy.TotalTimeout = 5 * time.Second
-	bin1 := aerospike.NewBin("firstbin", fmt.Sprintf("data"+time.Now().String()))
-	bin2 := aerospike.NewBin("secondbin", "data2")
-
-	err := client.PutBins(writePolicy, key, bin1, bin2)
-	if err != nil {
-		log.Println("1: ", err)
-	}
-	log.Println("Succesfully wrote record")
-
 	readPolicy := aerospike.NewPolicy()
 	readPolicy.TotalTimeout = 5 * time.Second
-	record, err := client.Get(readPolicy, key)
+	return &tester{
+		asClient:    client,
+		readPolicy:  readPolicy,
+		writePolicy: writePolicy,
+	}
+}
+
+func (t *tester) run() error {
+	start := time.Now().UTC()
+	defer log.Printf("Total time of program: %v", time.Since(start))
+	if seedData {
+		return t.seedData()
+	}
+	return t.testReadWriteConnectivity()
+}
+
+func (t *tester) seedData() error {
+	log.Println("seeding data")
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		time.Sleep(1 * time.Second) // wait a little bit to start next goroutine so we dont bombard the db all at once
+		go func(id int) {
+			t.createFakeData1kRecords(id, "users")
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (t *tester) createFakeData1kRecords(id int, setName string) {
+	const recordCount = 1000
+	setNamePerWorker := fmt.Sprintf("%d%s", id, setName)
+	log.Printf("threadID=%d creating %d records of data in set=%s\n", id, recordCount, setNamePerWorker)
+
+	binData := make([]byte, 1024)
+	if _, err := rand.Read(binData); err != nil {
+		log.Println("rand.Read err: ", err)
+	}
+
+	start := time.Now().UTC()
+
+	for i := 0; i < recordCount; i++ {
+		key := fmt.Sprintf("%d-%d-keyname", i, id)
+		asKey, err := aerospike.NewKey(namespace, setNamePerWorker, key)
+		if err != nil {
+			log.Println("newKey err: ", err)
+		}
+		bin := aerospike.NewBin("b", binData)
+		if err := t.asClient.PutBins(t.writePolicy, asKey, bin); err != nil {
+			log.Println("PutBins err: ", err)
+		}
+	}
+
+	fmt.Printf("threadID=%d  Time to create %d records: %s\n", id, recordCount, time.Since(start))
+}
+
+func (t *tester) testReadWriteConnectivity() error {
+	key, err := aerospike.NewKey(namespace, "foo", "bar")
 	if err != nil {
-		log.Println("2: ", err)
+		log.Println("newKey err: ", err)
+	}
+
+	for {
+		t.readWrite(key)
+		if !forever {
+			return nil
+		}
+		time.Sleep(15 * time.Second)
+	}
+}
+
+func (t *tester) readWrite(key *aerospike.Key) {
+	bin1 := aerospike.NewBin("b", fmt.Sprintf("data"+time.Now().String()))
+	bin2 := aerospike.NewBin("secondbin", "data2")
+
+	err := t.asClient.PutBins(t.writePolicy, key, bin1, bin2)
+	if err != nil {
+		log.Println("Put err: ", err)
+	} else {
+		log.Println("Succesfully wrote record")
+	}
+
+	record, err := t.asClient.Get(t.readPolicy, key)
+	if err != nil {
+		log.Println("Get err: ", err)
+		return
 	}
 
 	log.Printf("Reading record: key: %s, bin map:%v\n", record.Key, record.Bins)
--- readme.md
+++ readme.md
@@ -5,17 +5,37 @@ Test app that writes a single record to an Aerospike db then reads it back.
 ### Run app
 Set env vars:
 ```
-export AEROSPIKE_CLOUD_HOSTNAME=
-export AEROSPIKE_CLOUD_API_KEY_ID=
-export AEROSPIKE_CLOUD_API_KEY_SECRET=
+export AEROSPIKE_CLOUD_HOSTNAME=239f1a7c-0183-4de3-8407-172b833cadb9.aerospikedev.com
+export AEROSPIKE_CLOUD_API_KEY_ID=962c9a9589d4455fab8b2c6d42e21f0a
+export AEROSPIKE_CLOUD_API_KEY_SECRET=2f5466ae65c0479aa50cc21622873018
 ```
 
-Run app once:
+Build binary
 ```
-$ go run main.go
+$ go install
 ```
 
-Run app on a loop forever, sleeping every 15s between read/writes to the database:
+Usage:
 ```
-$ go run main.go --forever
+$ aerospike-app -help
+```
+
+Run app once which will read/write a single record to the database to check for connectivity:
+```
+$ aerospike-app
+```
+
+Run app on a loop forever, sleeping every 15s between read/writes to the database. Each loop writes a single record to the db at the same key:
+```
+$ aerospike-app -forever
+```
+
+Run app to seed a database with fake data, the default settings runs 10 threads. Each thread creates 1000 rows each with 1024 bytes of data for a total of 10 mb of data.
+```
+$ aerospike-app -seed-data
+```
+
+Increase how many threads are used to seed the database with more fake data. Increasing concurrency to 100 will seed the database with 100MB of data:
+```
+$ aerospike-app -seed-data -concurrency 100
 ```
